a27242df026b5a452592c7161582fe9035a50bae
rebsiot <rebsiot@gmail.com>
2026-05-11 10:39:24 -0500

    Update module.js

--- esmodules/module.js
+++ esmodules/module.js
@@ -3,34 +3,34 @@ import { libWrapper } from "./shim.js";
 const ROLES = Object.freeze(["none", "player", "trusted", "assistant", "gamemaster"]);
 
 Hooks.once("init", () => {
-    libWrapper.register("pf2e-karmic-dice", "CONFIG.Dice.termTypes.DiceTerm.prototype.roll", async function (wrapped, options) {
+    libWrapper.register("pf2e-karmic-dice-draft", "CONFIG.Dice.termTypes.DiceTerm.prototype.roll", async function (wrapped, options) {
         const oldRoll = await wrapped(options);
         const newRoll = JSON.parse(JSON.stringify(oldRoll));
 
-        if (this._root?.constructor.name === "CheckRoll" && game.settings.get("pf2e-karmic-dice", ROLES[game.user.role])) {
-            const userKarma = game.user.getFlag("pf2e-karmic-dice", "karma") ?? { history: [], cumulative: 0 };
+        if (this._root?.constructor.name === "CheckRoll" && game.settings.get("pf2e-karmic-dice-draft", ROLES[game.user.role])) {
+            const userKarma = game.user.getFlag("pf2e-karmic-dice-draft", "karma") ?? { history: [], cumulative: 0 };
 
             userKarma.history.push(oldRoll.result);
-            while (userKarma.history.length > game.settings.get("pf2e-karmic-dice", "history.value")) {
+            while (userKarma.history.length > game.settings.get("pf2e-karmic-dice-draft", "history.value")) {
                 userKarma.history.shift();
             }
 
-            if (this._root.options.heroPoint && game.settings.get("pf2e-karmic-dice", "heroPoint.enabled")) {
-                if (oldRoll.result <= game.settings.get("pf2e-karmic-dice", "heroPoint.threshold")) {
-                    newRoll.result = Math.clamp(oldRoll.result + game.settings.get("pf2e-karmic-dice", "heroPoint.value"), 1, this.faces);
+            if (this._root.options.heroPoint && game.settings.get("pf2e-karmic-dice-draft", "heroPoint.enabled")) {
+                if (oldRoll.result <= game.settings.get("pf2e-karmic-dice-draft", "heroPoint.threshold")) {
+                    newRoll.result = Math.clamp(oldRoll.result + game.settings.get("pf2e-karmic-dice-draft", "heroPoint.value"), 1, this.faces);
                 }
-            } else if (userKarma.history.length === game.settings.get("pf2e-karmic-dice", "history.value")) {
+            } else if (userKarma.history.length === game.settings.get("pf2e-karmic-dice-draft", "history.value")) {
                 const average = userKarma.history.reduce((a, b) => a + b, 0) / userKarma.history.length;
 
-                if (game.settings.get("pf2e-karmic-dice", "lowerThreshold.enabled") && average <= game.settings.get("pf2e-karmic-dice", "lowerThreshold.value")) {
-                    userKarma.cumulative = ((game.settings.get("pf2e-karmic-dice", "nudge.cumulative")) ? userKarma.cumulative + 1 : 1);
-                    const nudge = game.settings.get("pf2e-karmic-dice", "nudge.value") * userKarma.cumulative;
+                if (game.settings.get("pf2e-karmic-dice-draft", "lowerThreshold.enabled") && average <= game.settings.get("pf2e-karmic-dice", "lowerThreshold.value")) {
+                    userKarma.cumulative = ((game.settings.get("pf2e-karmic-dice-draft", "nudge.cumulative")) ? userKarma.cumulative + 1 : 1);
+                    const nudge = game.settings.get("pf2e-karmic-dice-draft", "nudge.value") * userKarma.cumulative;
                     newRoll.result = Math.clamp(oldRoll.result + nudge, 1, this.faces);
                 }
 
-                if (game.settings.get("pf2e-karmic-dice", "upperThreshold.enabled") && average >= game.settings.get("pf2e-karmic-dice", "upperThreshold.value")) {
-                    userKarma.cumulative = ((game.settings.get("pf2e-karmic-dice", "nudge.cumulative")) ? userKarma.cumulative + 1 : 1);
-                    const nudge = game.settings.get("pf2e-karmic-dice", "nudge.value") * userKarma.cumulative;
+                if (game.settings.get("pf2e-karmic-dice-draft", "upperThreshold.enabled") && average >= game.settings.get("pf2e-karmic-dice-draft", "upperThreshold.value")) {
+                    userKarma.cumulative = ((game.settings.get("pf2e-karmic-dice-draft", "nudge.cumulative")) ? userKarma.cumulative + 1 : 1);
+                    const nudge = game.settings.get("pf2e-karmic-dice-draft", "nudge.value") * userKarma.cumulative;
                     newRoll.result = Math.clamp(oldRoll.result - nudge, 1, this.faces);
                 }
             }
@@ -43,7 +43,7 @@ Hooks.once("init", () => {
                 userKarma.cumulative = 0;
             }
 
-            game.user.setFlag("pf2e-karmic-dice", "karma", userKarma);
+            game.user.setFlag("pf2e-karmic-dice-draft", "karma", userKarma);
         }
 
         this.results.pop();
@@ -51,7 +51,7 @@ Hooks.once("init", () => {
         return newRoll;
     });
 
-    game.settings.register("pf2e-karmic-dice", "player", {
+    game.settings.register("pf2e-karmic-dice-draft", "player", {
         name: "USER.RolePlayer",
         scope: "world",
         config: true,
@@ -59,7 +59,7 @@ Hooks.once("init", () => {
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "trusted", {
+    game.settings.register("pf2e-karmic-dice-draft", "trusted", {
         name: "USER.RoleTrusted",
         scope: "world",
         config: true,
@@ -67,7 +67,7 @@ Hooks.once("init", () => {
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "assistant", {
+    game.settings.register("pf2e-karmic-dice-draft", "assistant", {
         name: "USER.RoleAssistant",
         scope: "world",
         config: true,
@@ -75,7 +75,7 @@ Hooks.once("init", () => {
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "gamemaster", {
+    game.settings.register("pf2e-karmic-dice-draft", "gamemaster", {
         name: "USER.RoleGamemaster",
         scope: "world",
         config: true,
@@ -83,85 +83,85 @@ Hooks.once("init", () => {
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "history.value", {
-        name: "pf2e-karmic-dice.settings.history.value.name",
-        hint: "pf2e-karmic-dice.settings.history.value.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "history.value", {
+        name: "pf2e-karmic-dice-draft.settings.history.value.name",
+        hint: "pf2e-karmic-dice-draft.settings.history.value.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 2, max: 15, step: 1, initial: 5 })
     });
 
-    game.settings.register("pf2e-karmic-dice", "lowerThreshold.enabled", {
-        name: "pf2e-karmic-dice.settings.lowerThreshold.enabled.name",
-        hint: "pf2e-karmic-dice.settings.lowerThreshold.enabled.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "lowerThreshold.enabled", {
+        name: "pf2e-karmic-dice-draft.settings.lowerThreshold.enabled.name",
+        hint: "pf2e-karmic-dice-draft.settings.lowerThreshold.enabled.hint",
         scope: "world",
         config: true,
         type: Boolean,
         default: true,
     });
 
-    game.settings.register("pf2e-karmic-dice", "lowerThreshold.value", {
-        name: "pf2e-karmic-dice.settings.lowerThreshold.value.name",
-        hint: "pf2e-karmic-dice.settings.lowerThreshold.value.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "lowerThreshold.value", {
+        name: "pf2e-karmic-dice-draft.settings.lowerThreshold.value.name",
+        hint: "pf2e-karmic-dice-draft.settings.lowerThreshold.value.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 1, max: 20, step: 1, initial: 5 })
     });
 
-    game.settings.register("pf2e-karmic-dice", "upperThreshold.enabled", {
-        name: "pf2e-karmic-dice.settings.upperThreshold.enabled.name",
-        hint: "pf2e-karmic-dice.settings.upperThreshold.enabled.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "upperThreshold.enabled", {
+        name: "pf2e-karmic-dice-draft.settings.upperThreshold.enabled.name",
+        hint: "pf2e-karmic-dice-draft.settings.upperThreshold.enabled.hint",
         scope: "world",
         config: true,
         type: Boolean,
         default: true,
     });
 
-    game.settings.register("pf2e-karmic-dice", "upperThreshold.value", {
-        name: "pf2e-karmic-dice.settings.upperThreshold.value.name",
-        hint: "pf2e-karmic-dice.settings.upperThreshold.value.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "upperThreshold.value", {
+        name: "pf2e-karmic-dice-draft.settings.upperThreshold.value.name",
+        hint: "pf2e-karmic-dice-draft.settings.upperThreshold.value.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 1, max: 20, step: 1, initial: 15 })
     });
 
-    game.settings.register("pf2e-karmic-dice", "nudge.value", {
-        name: "pf2e-karmic-dice.settings.nudge.value.name",
-        hint: "pf2e-karmic-dice.settings.nudge.value.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "nudge.value", {
+        name: "pf2e-karmic-dice-draft.settings.nudge.value.name",
+        hint: "pf2e-karmic-dice-draft.settings.nudge.value.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 1, max: 10, step: 1, initial: 5 })
     });
 
-    game.settings.register("pf2e-karmic-dice", "nudge.cumulative", {
-        name: "pf2e-karmic-dice.settings.nudge.cumulative.name",
-        hint: "pf2e-karmic-dice.settings.nudge.cumulative.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "nudge.cumulative", {
+        name: "pf2e-karmic-dice-draft.settings.nudge.cumulative.name",
+        hint: "pf2e-karmic-dice-draft.settings.nudge.cumulative.hint",
         scope: "world",
         config: true,
         type: Boolean,
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "heroPoint.enabled", {
-        name: "pf2e-karmic-dice.settings.heroPoint.enabled.name",
-        hint: "pf2e-karmic-dice.settings.heroPoint.enabled.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "heroPoint.enabled", {
+        name: "pf2e-karmic-dice-draft.settings.heroPoint.enabled.name",
+        hint: "pf2e-karmic-dice-draft.settings.heroPoint.enabled.hint",
         scope: "world",
         config: true,
         type: Boolean,
         default: true
     });
 
-    game.settings.register("pf2e-karmic-dice", "heroPoint.value", {
-        name: "pf2e-karmic-dice.settings.heroPoint.value.name",
-        hint: "pf2e-karmic-dice.settings.heroPoint.value.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "heroPoint.value", {
+        name: "pf2e-karmic-dice-draft.settings.heroPoint.value.name",
+        hint: "pf2e-karmic-dice-draft.settings.heroPoint.value.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 1, max: 10, step: 1, initial: 10 })
     });
 
-    game.settings.register("pf2e-karmic-dice", "heroPoint.threshold", {
-        name: "pf2e-karmic-dice.settings.heroPoint.threshold.name",
-        hint: "pf2e-karmic-dice.settings.heroPoint.threshold.hint",
+    game.settings.register("pf2e-karmic-dice-draft", "heroPoint.threshold", {
+        name: "pf2e-karmic-dice-draft.settings.heroPoint.threshold.name",
+        hint: "pf2e-karmic-dice-draft.settings.heroPoint.threshold.hint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.NumberField({ min: 1, max: 20, step: 1, initial: 9 })
@@ -170,7 +170,7 @@ Hooks.once("init", () => {
 });
 
 Hooks.on("renderSettingsConfig", (_, html) => {
-    const tab = html.find('.tab[data-tab="pf2e-karmic-dice"]');
+    const tab = html.find('.tab[data-tab="pf2e-karmic-dice-draft"]');
 
     function beforeGroup(dataSettingID, key, dom = "h3") {
         tab.find(`.form-group[data-setting-id="${dataSettingID}"]`)
@@ -178,12 +178,12 @@ Hooks.on("renderSettingsConfig", (_, html) => {
     }
 
     if (game.user.isGM) {
-        beforeGroup("pf2e-karmic-dice.player", "pf2e-karmic-dice.headers.roles");
-        beforeGroup("pf2e-karmic-dice.history.value", "pf2e-karmic-dice.headers.history");
-        beforeGroup("pf2e-karmic-dice.lowerThreshold.enabled", "pf2e-karmic-dice.headers.lowerThreshold");
-        beforeGroup("pf2e-karmic-dice.upperThreshold.enabled", "pf2e-karmic-dice.headers.upperThreshold");
-        beforeGroup("pf2e-karmic-dice.nudge.value", "pf2e-karmic-dice.headers.nudge");
-        beforeGroup("pf2e-karmic-dice.heroPoint.enabled", "pf2e-karmic-dice.headers.heroPoints");
+        beforeGroup("pf2e-karmic-dice-draft.player", "pf2e-karmic-dice.headers.roles");
+        beforeGroup("pf2e-karmic-dice-draft.history.value", "pf2e-karmic-dice.headers.history");
+        beforeGroup("pf2e-karmic-dice-draft.lowerThreshold.enabled", "pf2e-karmic-dice.headers.lowerThreshold");
+        beforeGroup("pf2e-karmic-dice-draft.upperThreshold.enabled", "pf2e-karmic-dice.headers.upperThreshold");
+        beforeGroup("pf2e-karmic-dice-draft.nudge.value", "pf2e-karmic-dice.headers.nudge");
+        beforeGroup("pf2e-karmic-dice-draft.heroPoint.enabled", "pf2e-karmic-dice.headers.heroPoints");
     }
 });
 
@@ -198,7 +198,7 @@ Hooks.on("renderChatMessage", (message, html, data) => {
             for (const die of roll.dice) {
                 for (const result of die.results) {
                     if (Object.hasOwn(result, "originalResult")) {
-                        message.push(game.i18n.format("pf2e-karmic-dice.adjustRoll", { originalResult: result.originalResult, result: result.result }));
+                        message.push(game.i18n.format("pf2e-karmic-dice-draft.adjustRoll", { originalResult: result.originalResult, result: result.result }));
                     }
                 }
             }
